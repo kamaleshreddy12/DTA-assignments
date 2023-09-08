4ee4de660e4a803abdc9f9fc23487c3eb7949a55
kamaleshreddy12 <88228762+kamaleshreddy12@users.noreply.github.com>
2023-09-08 15:21:49 +0530

    Add files via upload

---
+++ que2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace assignmentq2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userid = "Kamal123";
+            string password = "kamal@123";
+
+            string guserid = Console.ReadLine(Console.WriteLine("Enter user id: "));
+            string gpassword = Console.ReadLine(Console.WriteLine("Enter password: "));
+
+            if (userid == guserid && password == gpassword)
+            {
+                Console.WriteLine("valid details");
+            }
+            else
+            {
+                Console.WriteLine("Invalid details");
+            }
+            Console.ReadLine();
+        }
+    }
+}
